6125b9c50ba06bf7058390a789d4bc7f7a0d2a8c
Cesar-Moran <cesarmorangithub@gmail.com>
2023-11-03 18:24:47 -0500

    Trying to fix navbar 3.0

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function DefaultNavbar() {
+function DefaultNavbar() {
   return (
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4 bg-white">
       <nav
@@ -33,7 +33,7 @@ export default function DefaultNavbar() {
                 viewBox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
                 />
               </svg>
@@ -108,3 +108,5 @@ export default function DefaultNavbar() {
     </header>
   );
 }
+
+export default DefaultNavbar;
